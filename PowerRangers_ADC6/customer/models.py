1eeeb312b3bab0a3be0415423c3e6ce37143b7a3
Roronish <58898924+Roronish@users.noreply.github.com>
2020-01-30 19:58:50 +0545

    Update models.py

--- PowerRangers_ADC6/customer/models.py
+++ PowerRangers_ADC6/customer/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
-from django.conf import *
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 class BookRoom(models.Model):
     cname = models.CharField(max_length=20)
@@ -19,3 +20,68 @@ class Hotel_Image(models.Model):
     hotel_image = models.URLField()
     hotel_name = models.CharField(max_length=50)
     hotel_description = models.CharField(max_length=70)
+
+
+# Stores all the hotel details and is used to query hotels.
+class Hotels(models.Model):
+    Name = models.CharField(max_length  = 255)
+    Address = models.CharField(max_length  = 255)
+    City = models.CharField(max_length  = 255)
+    Country = models.CharField(max_length  = 255)
+    TelephoneNumber = models.CharField(max_length=12)
+    ImagePath = models.CharField(max_length  = 255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now=True)
+    Description = models.TextField(max_length  = 140)
+    class Meta:
+        verbose_name_plural = 'Hotels'
+
+    def get_absolute_url(self):
+        return reverse('hoteldetails', kwargs={'pk': self.pk})
+    def __str__(self):
+         return self.Name
+# Stores the Hotel reviews and is used to query the reviews
+class Review(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    hotel = models.ForeignKey(Hotels,on_delete=models.CASCADE)
+    comment = models.CharField(max_length  = 255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    rating = models.IntegerField(default = 0)
+    class Meta:
+        verbose_name_plural = 'Review'
+
+    def __str__(self):
+         return self.comment
+# Stores the Hotels rooms
+class Room(models.Model):
+    hotel = models.ForeignKey(Hotels,on_delete=models.CASCADE)
+    RoomType = models.CharField(max_length  = 255)
+    Capacity = models.IntegerField(default = 0)
+    BedOption = models.CharField(max_length  = 255)
+    Price= models.IntegerField(default = 0)
+    View = models.CharField(max_length  = 255)
+    TotalRooms = models.CharField(max_length  = 255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        verbose_name_plural = 'Room'
+
+    def __str__(self):
+         return self.RoomType
+# Stores and is used to query partner proposals
+class Proposal(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    CompanyName = models.CharField(max_length  = 255)
+    CompanyEmail = models.EmailField(max_length  = 254)
+    HQAddress = models.CharField(max_length  = 255)
+    Vision = models.TextField(max_length  = 400)
+    class Meta:
+        verbose_name_plural = 'Proposal'
+
+    def __str__(self):
+         return self.CompanyName
+
+
+
+
