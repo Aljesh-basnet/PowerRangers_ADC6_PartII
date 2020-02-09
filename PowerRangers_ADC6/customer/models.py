c0cef0a9a05a471e6862f244e79b941ca5167d08
Aljesh-basnet <58818892+Aljesh-basnet@users.noreply.github.com>
2020-02-09 23:24:14 +0545

    model relation and test cases

--- PowerRangers_ADC6/customer/models.py
+++ PowerRangers_ADC6/customer/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.conf import settings
 from django.conf import *
+from django.contrib.auth.models import User
+from django.urls import reverse
+import datetime
 
 class BookRoom(models.Model):
     cname = models.CharField(max_length=20)
@@ -12,6 +15,47 @@ class BookRoom(models.Model):
     
     def __str__(self):
         return f"{self.cname} has booked {self.roomno}"
+
+
+
+# Stores all the hotel details and is used to query hotels.
+class Hotels(models.Model):
+    Name = models.CharField(max_length  = 255)
+    Address = models.CharField(max_length  = 255)
+    City = models.CharField(max_length  = 255)
+    TelephoneNumber = models.CharField(max_length=12)
+    ImagePath = models.CharField(max_length  = 255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now=True)
+    Description = models.TextField(max_length  = 140)
+    class Meta:
+        verbose_name_plural = 'Hotels'
+
+    def get_absolute_url(self):
+        return reverse('hoteldetails', kwargs={'pk': self.pk})
+    def __str__(self):
+         return self.Name
+
+# Stores the Hotels rooms
+class Room(models.Model):
+    hotel = models.ForeignKey(Hotels,on_delete=models.CASCADE)
+    RoomType = models.CharField(max_length  = 255)
+    Capacity = models.IntegerField(default = 0)
+    BedOption = models.CharField(max_length  = 255)
+    Price= models.IntegerField(default = 0)
+    View = models.CharField(max_length  = 255)
+    TotalRooms = models.CharField(max_length  = 255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        verbose_name_plural = 'Room'
+
+    def __str__(self):
+         return self.RoomType
+
+
+
+
 
 
 
