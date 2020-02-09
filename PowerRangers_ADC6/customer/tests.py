63399f310e95332b2331ef1f1c2198dbef491a2a
Roronish <58898924+Roronish@users.noreply.github.com>
2020-02-09 19:55:04 +0545

    Update tests.py

--- PowerRangers_ADC6/customer/tests.py
+++ PowerRangers_ADC6/customer/tests.py
@@ -4,24 +4,23 @@ from django.contrib.auth.models import User
 
 class ModelTestCase(TestCase):       
         def setUp(self):
-            hostel = Hotels.objects.create(Name="Your Dream Hotel")
+            Hotels = Hotels.objects.create(Name="Your Dream Hotel")
             location = Hotels.objects.create(Location="Chitwan")
 
-
         def test_book_name(self):
-        book1=BookRoom.objects.get(name="Aljesh")
-        self.assertEqual(books.name,"Aljesh")
+            book1=BookRoom.objects.get(name="Aljesh")
+            self.assertEqual(book1.name,"Aljesh")
     
         def test_valid_phone(self):
-        book2=BookRoom.objects.get(phone="9866313335")
-        value=book2.is_valid_phone()
-        self.assertFalse(value,True)
+            book2=BookRoom.objects.get(phone="9866313335")
+            value=book2.is_valid_phone()
+            self.assertFalse(value,True)
 
         def test_valid_bookroom(self):
-        b = BookRoom.objects.get(arrival="2016-02-25",checkOut="2018-03-25")
-        value=b.is_valid_book()
-        self.assertTrue(b,True)
+            b = BookRoom.objects.get(arrival="2016-02-25",checkOut="2018-03-25")
+            value=b.is_valid_book()
+            self.assertTrue(b,True)
 
         def test_description(self):
-        description = Hotels.objects.get(Description = 'Welcome to Our Hotel')
-        self.assertEqual(description.Description, 'Welcome to Our Hotel') 
+            description = Hotels.objects.get(Description = 'Welcome to Our Hotel')
+            self.assertEqual(description.Description, 'Welcome to Our Hotel') 
